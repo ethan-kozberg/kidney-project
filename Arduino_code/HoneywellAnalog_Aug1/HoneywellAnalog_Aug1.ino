c0bcc133328c8590c02f1a36b66dbbcc9e7f94da
ethan-kozberg <ethankozberg@gmail.com>
2025-08-06 14:02:00 -0700

    new honeywell

---
+++ Arduino_code/HoneywellAnalog_Aug1/HoneywellAnalog_Aug1.ino
@@ -0,0 +1,14 @@
+#include<Arduino.h>
+#include<wire.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
