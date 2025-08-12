b5a8afef64316881f936dd610ad44dfb2ef7b1ab
ethan-kozberg <ethankozberg@gmail.com>
2025-08-11 19:24:13 -0700

    change ref voltage to meter

--- Arduino_code/HoneywellAnalog_Aug1/HoneywellAnalog_Aug1.ino
+++ Arduino_code/HoneywellAnalog_Aug1/HoneywellAnalog_Aug1.ino
@@ -3,7 +3,7 @@
 #include<limits.h>
 
 // Constants
-const float V_REF = 5.0;     // Analog reference voltage (e.g., 5V or 3.3V)
+const float V_REF = 5.172;     // Analog reference voltage (e.g., 5V or 3.3V)
 const float R_BITS = 10.0;   // ADC resolution (bits)
 const float ADC_STEPS = (1 << int(R_BITS)) - 1; // Number of steps (2^R_BITS - 1)
 
@@ -15,7 +15,8 @@ float Omax = 0.9*V_REF; // Output at maximum pressure
 float Omin = 0.1*V_REF; // Output at minimum pressure
 float Pmax = 1*51.7149; // 1 psi to mmhg
 float Pmin = -1*51.7149; // -1 psi to mmhg
-float Poffset = 0; // offset from zero
+float Poffset = 15; // offset from zero
+int once_counter = 0;
 
 // Setup pressure sensor zero
 int incomingByte = 0;
@@ -27,7 +28,7 @@ void setup() {
 }
 
 void loop() {
-  Poffset = 8.531;
+  
   // put your main code here, to run repeatedly:
   int rawValue = analogRead(analogPin); // Read the analog input
   float voltage = (rawValue / ADC_STEPS) * V_REF; // Convert to voltage
@@ -46,18 +47,25 @@ void loop() {
   
 
   // check if zero button is pressed
-  if (Serial.available() > 0) {
+  //if (Serial.available() > 0) {
 
     // read the incoming byte:
-    incomingByte = Serial.read();
+  //  incomingByte = Serial.read();
     // say what you got:
-    Serial.print("I received: ");
-    Serial.println(incomingByte, DEC);
-    Poffset = pressure;
-  }
+  //  Serial.print("I received: ");
+  //  Serial.println(incomingByte, DEC);
+  //  Poffset = pressure;
+  //}
+
+  // instant zeroing
+  //if(once_counter < 1) {
+  //  Poffset = pressure;
+  //}
+  //once_counter = 1;
+
 
   // if button is pressed (eg digital high)
   // then set poffset to current pressure
   
-  delay(3000); // 3 sec delay
+  delay(10); // 3 sec delay
 }
